3d7dc14a900fc580294607aef5bca9bf13934e3c
Varun <varunv@hifx.co.in>
2016-07-27 11:04:27 +0530

    redis dial options added

--- infra/redis/redis.go
+++ infra/redis/redis.go
@@ -34,13 +34,13 @@ func Connect(host string, maxactive, maxidle int) (*redis.Pool, error) {
 }
 
 //AuthConnect connects and authenticated with the password
-func AuthConnect(host string, password string, maxactive, maxidle int) (*redis.Pool, error) {
+func AuthConnect(host string, password string, maxactive, maxidle int, options ...redis.DialOption) (*redis.Pool, error) {
 	pool := &redis.Pool{
 		MaxIdle:     maxidle,
 		MaxActive:   maxactive,
 		IdleTimeout: IDLE_TIMEOUT,
 		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", host)
+			c, err := redis.Dial("tcp", host, options...)
 			if err != nil {
 				return nil, err
 			}

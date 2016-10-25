209d813ac3cd4a2a6238b54d00cea483ca60a4d8
zak <zak@hifx.co.in>
2016-10-25 17:04:01 +0530

    aws-sdk-go beta version

--- infra/aws_sdk_go/aws_sdk_go.go
+++ infra/aws_sdk_go/aws_sdk_go.go
@@ -7,30 +7,26 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
-	"fmt"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
 )
 
 func ConfigAws(regionName, accessKey, secretKey string, doIam bool) (*aws.Config, error) {
-
-	awsConf := &aws.Config{Region: aws.String(regionName)}
-
-	if !doIam {
-		if len(accessKey) == 0 || len(secretKey) == 0 {
-			return nil,fmt.Errorf("ses access key and/or secret key is/are blank")
-		} else {
-			token := ""
-			creds := credentials.NewStaticCredentials(accessKey, secretKey, token)
-			_, err := creds.Get()
-			if err != nil {
-				return nil,err
-			}
-			awsConf = &aws.Config{
-				Region: aws.String(regionName),
-				Credentials: creds}
-		}
-	}
-	return awsConf,nil
+	sess:= session.New()
+	awsConfig := aws.NewConfig().WithCredentials(credentials.NewChainCredentials([]credentials.Provider{
+		&ec2rolecreds.EC2RoleProvider{
+			Client: ec2metadata.New(sess),
+		},
+		&credentials.StaticProvider{
+			Value: credentials.Value{
+				AccessKeyID:     accessKey,
+				SecretAccessKey: secretKey,
+				SessionToken:    "",
+			},
+		},
+	})).WithRegion(regionName)
+	return awsConfig,nil
 }
 
 func ConnectSns(config *aws.Config) (*sns.SNS, error) {
